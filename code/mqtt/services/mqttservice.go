2634af97ea6fc54683960033353289a27a35a1b5
aproperty <2483777043@qq.com>
2022-11-18 20:31:57 +0800

    .

---
+++ code/mqtt/services/mqttservice.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"fmt"
+
+	"temporary/mqtt"
+
+	gomqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func SubscribeData() {
+
+	item := mqtt.SubscribeType{
+		Topic:      "topic_event",
+		Qos:        byte(2),
+		Callback:   TestMessageHandler,
+		RetryTimes: 0,
+	}
+
+	// 注册需要订阅的消息，连接后会自动订阅
+	mqtt.AppendToSubscribers(item)
+
+}
+
+// TestMessageHandler 函数定义格式如下
+func TestMessageHandler(client gomqtt.Client, msg gomqtt.Message) {
+
+	fmt.Printf("fmt - Subscribe - TOPIC: %s  MSG: %s",
+		msg.Topic(),
+		msg.Payload())
+}
